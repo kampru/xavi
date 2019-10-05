fa8e1a4088da3a696b0eb6e81028b9f0d9016dda
evaBarbeito <ebarbeito@correu.escoladeltreball.org>
2019-10-05 17:32:38 +0200

    sample code

---
+++ app/src/main/java/com/dam/eva/pt1/HelpActivity.java
@@ -0,0 +1,26 @@
+package com.dam.eva.pt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.txtViewHelp);
+        //textView.setText(message);
+
+    }
+}
---
+++ app/src/main/java/com/dam/eva/pt1/MainActivity.java
@@ -0,0 +1,78 @@
+package com.dam.eva.pt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    // TODO: 1/10/19
+
+    public static final String EXTRA_MESSAGE="com.dam.eva.pt1";
+    public static final String TAG="MainActivity";
+
+    Button button;
+    EditText edtNom;
+    Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button= (Button) findViewById(R.id.btnBoto);
+        button2= (Button) findViewById(R.id.btnBoto2 );
+        edtNom=(EditText) findViewById(R.id.edtNom);
+
+
+        Log.d(TAG,"debugging");
+        Toast.makeText(this,"primera app",Toast.LENGTH_LONG).show();
+
+        /*button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(this,"primera app",Toast.LENGTH_LONG).show();
+                Log.d(TAG,"boto2");
+
+            }
+        });*/
+    }
+
+    public void onClickHelp(View view){
+
+
+        Intent intent= new Intent(this, HelpActivity.class);
+
+
+        String message=edtNom.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
+
+        Bundle b=new Bundle();
+        b.putString("NOM",message);
+        intent.putExtras(b);
+
+        startActivity(intent);
+
+    }
+
+    public void onClickCreaCompte(View v){
+
+
+        String nomUser=edtNom.getText().toString();
+
+        Toast.makeText(this, "Crearà una nova compta per a:"
+                +  nomUser, Toast.LENGTH_LONG).show();
+        Log.d(TAG,"crearà nova compta");
+        TextView textView= (TextView) findViewById(R.id.txtView1);
+        textView.setText(nomUser);
+
+    }
+
+}
+
